03303c50ae7f6c84cedfa08245aab68a79ae1bf4
hungnv <hungnvcntt91@gmail.com>
2021-03-09 10:57:44 +0700

    Fix metadata

--- src/main/java/com/yody/common/core/dto/PageableDto.java
+++ src/main/java/com/yody/common/core/dto/PageableDto.java
@@ -1,14 +1,18 @@
 package com.yody.common.core.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 @Data
 public class PageableDto {
     private Metadata metadata;
     private Object items;
+
+    @Data
+    @AllArgsConstructor
+    public class Metadata{
+        private int total;
+        private int page;
+        private int limit;
+    }
 }
-@Data
-class Metadata{
-    private int total;
-    private int page;
-    private int limit;
-}
+
