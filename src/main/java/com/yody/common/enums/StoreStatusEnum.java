b3ddc20327e8102b1dd1b976712ce90d91c3ceb6
Anh Nguyen <anhnn2@dtsgroup.com.vn>
2021-03-11 10:53:40 +0700

    add status store enum

---
+++ src/main/java/com/yody/common/enums/StoreStatusEnum.java
@@ -0,0 +1,24 @@
+package com.yody.common.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreStatusEnum implements BaseEnum<Integer> {
+    ACTIVE(1, "Đang hoạt động"),
+    LOCK(2, "Khóa tạm thời"),
+    BLOCKED(3, "Khóa vĩnh viễn"),
+    INVENTORY_CHECKING(4, "Đang kiểm kho");
+
+    private final int value;
+    private final String displayName;
+    @Override
+    public Integer getValue() {
+        return value;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return displayName;
+    }
+}
+
