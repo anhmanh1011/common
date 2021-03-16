b86798968ed61ec6919ffb9b6673582ba1986e98
hungnv <hungnvcntt91@gmail.com>
2021-03-16 09:12:03 +0700

    update Base exception

--- src/main/java/com/yody/common/core/BaseEntity.java
+++ src/main/java/com/yody/common/core/BaseEntity.java
@@ -1,13 +1,10 @@
 package com.yody.common.core;
 
 
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import lombok.Data;
 
 @Data
@@ -17,7 +14,7 @@ public class BaseEntity {
     @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
     private boolean isDeleted;
 
-    @Column(name = "version", columnDefinition = "smallint",nullable = false)
+    @Column(name = "version", columnDefinition = "smallint", nullable = false)
     protected int version;
 
     @Column(name = "created_by", nullable = false, length = 50)
@@ -38,7 +35,6 @@ public class BaseEntity {
     @Column(name = "updated_date")
     protected Long updatedDate;
 
-
     @PrePersist
     protected void onCreate() {
         this.version = 1;
@@ -48,7 +44,6 @@ public class BaseEntity {
     @PreUpdate
     protected void onUpdate() {
         this.updatedDate = System.currentTimeMillis();
-        this.version +=1;
+        this.version += 1;
     }
-
 }
