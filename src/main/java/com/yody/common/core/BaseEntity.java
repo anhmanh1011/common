86adc913d1ca61a62828339f356de494c2668de1
Nguyen Hung <hungnvcntt91@gmail.com>
2021-04-20 11:52:53 +0700

    remove flag

--- src/main/java/com/yody/common/core/BaseEntity.java
+++ src/main/java/com/yody/common/core/BaseEntity.java
@@ -1,8 +1,12 @@
 package com.yody.common.core;
 
-
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,52 +14,47 @@ import lombok.Setter;
 @Setter
 @MappedSuperclass
 public class BaseEntity {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  protected Long id;
 
-    @Column(name = "code", nullable = false, length = 36)
-    protected String code;
+  @Column(name = "code", nullable = false, length = 36)
+  protected String code;
 
-    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
-    protected boolean isDeleted;
+  @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
+  protected boolean isDeleted;
 
-    @Column(name = "version", columnDefinition = "smallint", nullable = false)
-    protected int version;
+  @Column(name = "version", columnDefinition = "smallint", nullable = false)
+  protected int version;
 
-    @Column(name = "created_by", nullable = false, length = 36)
-    protected String createdBy;
+  @Column(name = "created_by", nullable = false, length = 36)
+  protected String createdBy;
 
-    @Column(name = "created_name", nullable = false)
-    protected String createdName;
+  @Column(name = "created_name", nullable = false)
+  protected String createdName;
 
-    @Column(name = "updated_by", nullable = false, length = 36)
-    protected String updatedBy;
+  @Column(name = "updated_by", nullable = false, length = 36)
+  protected String updatedBy;
 
-    @Column(name = "updated_name", nullable = false)
-    protected String updatedName;
+  @Column(name = "updated_name", nullable = false)
+  protected String updatedName;
 
-    @Column(name = "created_date")
-    protected Long createdDate;
+  @Column(name = "created_date")
+  protected Long createdDate;
 
-    @Column(name = "updated_date")
-    protected Long updatedDate;
+  @Column(name = "updated_date")
+  protected Long updatedDate;
 
-    protected boolean selfUpdateVersion;
+  @PrePersist
+  protected void onCreate() {
+    this.version = 1;
+    this.createdDate = this.updatedDate = System.currentTimeMillis();
+  }
 
-    @PrePersist
-    protected void onCreate() {
-        this.version = 1;
-        this.createdDate = this.updatedDate = System.currentTimeMillis();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedDate = System.currentTimeMillis();
-        if(!selfUpdateVersion){
-            this.version++;
-        }
-
-    }
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedDate = System.currentTimeMillis();
+    this.version++;
+  }
 }
