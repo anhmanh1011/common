eb71315bf7e47be90a49c336e52cc0ae833f9a05
Nguyen Hung <hungnvcntt91@gmail.com>
2021-05-18 11:35:13 +0700

    add root

--- src/main/java/com/yody/common/core/BaseBO.java
+++ src/main/java/com/yody/common/core/BaseBO.java
@@ -1,11 +1,14 @@
 package com.yody.common.core;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.yody.common.core.domain.AggregateRoot;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
-public class BaseBO extends BaseEntity{
+public class BaseBO<T extends AggregateRoot<T>> extends BaseEntity{
+    @JsonIgnore
+    protected  T root;
     @JsonIgnore
     @Setter(AccessLevel.PROTECTED)
     protected boolean create;
