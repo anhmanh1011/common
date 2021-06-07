57c012cc2b18c35e4ee682f524814cc82c005776
Nguyen Hung <hungnvcntt91@gmail.com>
2021-06-07 10:08:58 +0700

    update base search

--- src/main/java/com/yody/common/core/BaseSearchRequest.java
+++ src/main/java/com/yody/common/core/BaseSearchRequest.java
@@ -1,11 +1,17 @@
 package com.yody.common.core;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.io.Serializable;
+import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class BaseSearchRequest implements Serializable {
     private int page;
     private int limit;
+    private Date fromDate;
+    private Date toDate;
 }
