4ba69d12b36982e918c868c071d8a6711303e9a7
pirest89 <thangvm6@onemount.com>
2021-07-05 14:00:30 +0700

    add swagger ui

--- src/main/java/com/yody/common/filter/HandlerFilter.java
+++ src/main/java/com/yody/common/filter/HandlerFilter.java
@@ -81,7 +81,7 @@ public class HandlerFilter implements Filter {
     try {
       HttpServletResponse response = (HttpServletResponse) servletResponse;
       HttpServletRequest request = (HttpServletRequest) servletRequest;
-      if (request.getRequestURI().contains("/v2/api-docs") || request.getRequestURI().contains("/actuator/health") || request.getRequestURI().contains("/actuator/info") || request
+      if (request.getRequestURI().contains("/v2/api-docs") || request.getRequestURI().contains("swagger-ui.html") || request.getRequestURI().contains("/actuator/health") || request.getRequestURI().contains("/actuator/info") || request
           .getRequestURI().contains("/accounts/login") || request.getMethod().equalsIgnoreCase("OPTIONS")) {
         filterChain.doFilter(servletRequest, servletResponse);
         return;
