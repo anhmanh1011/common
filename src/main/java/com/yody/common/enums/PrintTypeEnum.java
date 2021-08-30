3b430f8f8f57e950ffca50ce5fb32852b7bbdb32
ToanCV <toancv.it@tripi.vn>
2021-08-30 09:10:20 +0700

    add logo prview, print type

---
+++ src/main/java/com/yody/common/enums/PrintTypeEnum.java
@@ -0,0 +1,23 @@
+package com.yody.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum PrintTypeEnum implements BaseEnum<String> {
+    ORDER("order", "Hoá đơn bán hàng"),
+    PURCHASE_ORDER("purchase_order", "Hoá đơn nhập hàng");
+
+    private String  value;
+    private String displayName;
+
+    public static PrintTypeEnum parse(String value) {
+        for (PrintTypeEnum type : PrintTypeEnum.values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        return null;
+    }
+}
