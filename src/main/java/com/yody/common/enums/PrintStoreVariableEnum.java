8a572ddbf33e89705904454800e3e393ccec2656
ToanCV <toancv.it@tripi.vn>
2021-08-30 09:19:56 +0700

    update logo url

--- src/main/java/com/yody/common/enums/PrintStoreVariableEnum.java
+++ src/main/java/com/yody/common/enums/PrintStoreVariableEnum.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum PrintStoreVariableEnum implements BasePrintEnum<String> {
-    COMPANY_LOGO("{company_logo}", "Logo doanh nghiệp", "<img src=\"https://storage.googleapis.com/cdn.nhanh.vn/store/3138/logo_1615426885_logo-yody.png\" width=\"150\" height=\"70\">"),
+    COMPANY_LOGO("{company_logo}", "Logo doanh nghiệp", "<img src=\"https://cdn.yody.io/yody_official_logo.png\" width=\"150\" height=\"70\">"),
     COMPANY_NAME("{company_name}", "Tên doanh nghiệp", "Công ty cổ phần thời trang YODY"),
     COMPANY_ADDRESS("{company_address}", "Địa chỉ doanh nghiệp",
         "An Định, Việt Hoà, Thành phố Hải Dương, Hải Dương 03125"),
     COMPANY_PHONE("{company_phone}", "Số điện thoại doanh nghiệp", "1800 2086"),
-    STORE_LOGO("{store_logo}", "Logo cửa hàng", "<img src=\"https://img.jamja.vn/jamja-prod/gcs_full_58ab0f6b76ec57406acc4f15-2017-02-20-154652.png\" width=\"150\" height=\"70\">\""),
+    STORE_LOGO("{store_logo}", "Logo cửa hàng", "<img src=\"https://cdn.yody.io/yody_logo.png\" width=\"150\" height=\"70\">\""),
     STORE_NAME("{store_name}", "Tên cửa hàng", "Cửa hàng thời trang YODY"),
     STORE_ADDRESS("{store_address}", "Địa chỉ cửa hàng",
         "61 Trần Hưng Đạo, P. Trần Hưng Đạo, Thành phố Hải Dương, Hải Dương 170000"),
