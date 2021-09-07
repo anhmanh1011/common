d8d6a84f2a77a27cdee09f446b790d7e511ad891
ToanCV <toancv.it@tripi.vn>
2021-09-07 14:58:46 +0700

    add enums, utils

--- src/main/java/com/yody/common/utility/MapperUtils.java
+++ src/main/java/com/yody/common/utility/MapperUtils.java
@@ -3,12 +3,7 @@ package com.yody.common.utility;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
-import java.util.Base64;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 
 public class MapperUtils {
 
@@ -41,21 +36,6 @@ public class MapperUtils {
         try {
             return getObjectMapper().writeValueAsString(o);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public static BigDecimal stringToDecimal(String s) {
-        if (StringUtils.isBlank(s)) {
-            return null;
-        }
-        if (NumberUtils.isCreatable(s)) {
-            return new BigDecimal(s).setScale(2, RoundingMode.HALF_UP);
-        }
-        try {
-            return new BigDecimal(new BigInteger(Base64.getDecoder().decode(s)), 2);
-        } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return null;
         }
