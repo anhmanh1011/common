41ba4ec61dc3f63eb39793e88ff379efea9e9266
ToanCV <toancv.it@tripi.vn>
2021-09-24 12:33:05 +0700

    update mapper utils

--- src/main/java/com/yody/common/utility/MapperUtils.java
+++ src/main/java/com/yody/common/utility/MapperUtils.java
@@ -7,37 +7,46 @@ import org.apache.commons.lang3.StringUtils;
 
 public class MapperUtils {
 
-    private static ObjectMapper objectMapper = null;
+  private static ObjectMapper objectMapper = null;
 
-    private MapperUtils() {
-    }
+  private MapperUtils() {
+  }
 
-    public static ObjectMapper getObjectMapper() {
-        if (objectMapper == null) {
-            objectMapper = new ObjectMapper().configure(
-                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        }
-        return objectMapper;
+  public static ObjectMapper getObjectMapper() {
+    if (objectMapper == null) {
+      objectMapper = new ObjectMapper().configure(
+          DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
+    return objectMapper;
+  }
 
-    public static <T> T jsonToObject(String source, Class<T> target) {
-        if (StringUtils.isBlank(source)) {
-            return null;
-        }
-        try {
-            return getObjectMapper().readValue(source, target);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+  public static <T> T jsonToObject(String source, Class<T> target) {
+    if (StringUtils.isBlank(source)) {
+      return null;
     }
+    try {
+      return getObjectMapper().readValue(source, target);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 
-    public static String objectToJson(Object o) {
-        try {
-            return getObjectMapper().writeValueAsString(o);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return null;
-        }
+  public static String objectToJson(Object o) {
+    try {
+      return getObjectMapper().writeValueAsString(o);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+      return null;
     }
+  }
+
+  public static <T> T convert(Object fromValue, Class<T> toValueType) {
+    try {
+      return getObjectMapper().convertValue(fromValue, toValueType);
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
