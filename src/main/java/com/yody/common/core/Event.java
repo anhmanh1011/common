77a4047cd08049b72f91464bd3b6faf5c5639c41
ToanCV <toancv.it@tripi.vn>
2022-03-17 16:36:38 +0700

    add noargconstructor for event

--- src/main/java/com/yody/common/core/Event.java
+++ src/main/java/com/yody/common/core/Event.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Date;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Event<T>  {
   private String name;
   private T payload;
