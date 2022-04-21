da40f9e09548e5f4c718c6523751ccb0a40058ee
caothanh <caothanh651998@gmail.com>
2022-04-21 16:58:38 +0700

    update phieu bao hanh

---
+++ src/main/java/com/yody/common/enums/PrintWarrantyVariableEnum.java
@@ -0,0 +1,28 @@
+package com.yody.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum PrintWarrantyVariableEnum implements BasePrintEnum<String> {
+  APPOINTMENT_DATE("{appointment_date}", "Ngày hẹn trả", "21/04/22 10:11"),
+  REASON_WARRANTY("{reason_warranty}", "Lý do bảo hành", "May lại đường chỉ"),
+  WARRANTY_INDEX("warranty_index","Số lượng","1"),
+  PRODUCT_WARRANTY("product_warranty","Sản phẩm","áo Yody"),
+  PRODUCT_PRICE_WARRANTY("product_price_warranty","Giá","100.000"),
+  ;
+
+  private final String value;
+  private final String displayName;
+  private final String previewValue;
+
+  public static PrintWarrantyVariableEnum parse(String value) {
+    for (PrintWarrantyVariableEnum type : PrintWarrantyVariableEnum.values()) {
+      if (type.getValue().equals(value)) {
+        return type;
+      }
+    }
+    return null;
+  }
+}
